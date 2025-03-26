4aa3a19805811d672fb108df8fd0c505fec36d31
a1163123 <a1163123@gmail.com>
2025-03-27 01:12:08 +0900

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
    theme: {
     extend: {
       keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-60px)' }, // ← 커졌음!
+        },
+        drift: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-50px)' }, // ← 커졌음!
+        },
         micPulse: {
           '0%': { transform: 'translate(-50%, -50%) scale(1)', opacity: '0.4' },
           '100%': { transform: 'translate(-50%, -50%) scale(2)', opacity: '0' },
@@ -11,6 +19,8 @@ module.exports = {
       },
       animation: {
         micPing: 'micPulse 1.5s ease-out infinite',
+        float: 'float 10s ease-in-out infinite',
+        drift: 'drift 7s ease-in-out infinite',
       },
     },
   },
